5c31f24833884db97f3b1f6f069d991ea395d337
Scott B <Scott@TheBaileyFamily.org>
2022-05-05 15:18:46 -0700

    Ensure build is Release. (#3)
    * Ensure build is Release.
    
    * Added tests for header files.
    
    Co-authored-by: Avalon <avalon@anaconda.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-mkdir build && cd build
 
-cmake ${CMAKE_ARGS} \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_INCLUDEDIR=include \
-    -DENABLE_COMMONCRYPTO=OFF \
-    -DENABLE_GNUTLS=OFF \
-    -DENABLE_MBEDTLS=OFF \
-    -DENABLE_OPENSSL=ON \
-    -DENABLE_WINDOWS_CRYPTO=OFF \
-    -DENABLE_BZIP2=ON \
-    -DENABLE_LZMA=OFF \
-    -DENABLE_ZSTD=OFF \
-    ..
-make
-make test
-make install
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
+
+
+# Generate the build files.
+echo "Generating the build files."
+cmake .. ${CMAKE_ARGS} \
+      -GNinja \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DENABLE_COMMONCRYPTO=OFF \
+      -DENABLE_GNUTLS=OFF \
+      -DENABLE_MBEDTLS=OFF \
+      -DENABLE_OPENSSL=ON \
+      -DENABLE_WINDOWS_CRYPTO=OFF \
+      -DENABLE_BZIP2=ON \
+      -DENABLE_LZMA=OFF \
+      -DENABLE_ZSTD=OFF \
+      -DCMAKE_BUILD_TYPE=Release
+
+
+# Build.
+echo "Building..."
+ninja || exit 1
+
+
+# Perform tests.
+echo "Testing..."
+ninja test || exit 1
+#  path_to/test || exit 1
+#  ctest -VV --output-on-failure || exit 1
+
+
+# Installing
+echo "Installing..."
+ninja install || exit 1
+
+
+# Error free exit!
+echo "Error free exit!"
+exit 0
