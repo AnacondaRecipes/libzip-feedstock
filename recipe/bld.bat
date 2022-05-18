36a9000f34b8961292d1b974340119536eb1b3f6
ben <bferch@anaconda.com>
2022-05-18 15:30:26 -0700

    fixed cmake args in bld.bat to match build.sh

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,5 @@
 :: cmd
 
-:: Delete tests.
-:: Many of these seem to be failing due to UTF-8 issues - may want to look into later.
-del regress\clone-buffer-add.test
-del regress\clone-buffer-replace.test
-del regress\file_comment_encmismatch.test
-del regress\fseek_deflated.test
-del regress\fseek_fail.test
-del regress\preload.test
-del regress\rename_cp437.test
-del regress\rename_utf8.test
-del regress\rename_utf8_encmismatch.test
-del regress\set_file_mtime.test
-del regress\utf-8-standardization.test
-del regress\zip64_stored_creation.test
-
 :: Isolate the build.
 mkdir Build
 cd Build
@@ -27,9 +12,15 @@ cmake .. %CMAKE_ARGS% ^
       -G"Ninja" ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_BUILD_TYPE=Release ^
+      -DENABLE_COMMONCRYPTO=OFF ^
+      -DENABLE_GNUTLS=OFF ^
+      -DENABLE_MBEDTLS=OFF ^
+      -DENABLE_OPENSSL=ON ^
+      -DENABLE_WINDOWS_CRYPTO=OFF ^
       -DENABLE_BZIP2=ON ^
-      -DENABLE_LZMA=ON
+      -DENABLE_LZMA=ON ^
+      -DENABLE_ZSTD=ON ^
+      -DCMAKE_BUILD_TYPE=Release ^
 
 
 :: Build.
